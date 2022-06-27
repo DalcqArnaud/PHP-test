d075d66f4ff92a9e0485a46d8e57d4d1a8f3c51e
ArnaudDalcq <aranaud.dalcq@gmail.com>
2022-06-27 18:27:41 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php">Page 1</a>
+    <a href="cats.php">Page 2</a>
+    <?php 
+        echo "Page 1";
+        echo "<img src='images/cat1' alt='first cat photo' />";
+    ?>
+</body>
+</html>
